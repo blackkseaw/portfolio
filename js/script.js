13fcf7ba19e4003887342e1e924ad7f0143e893c
blackkseaw <blackkseaw@gmail.com>
2022-03-05 12:00:30 -0300

    03/05 - Skills card

---
+++ js/script.js
@@ -0,0 +1,58 @@
+AOS.init();
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     var tl = gsap.timeline();
+
+//     tl
+//         .from("nav", {
+//             duration: 1,
+//             y: 50,
+//             opacity: 0,
+//             delay: 0.5,
+//             ease: "power3.out",
+//         })
+
+        
+//     gsap.registerPlugin(ScrollTrigger);
+
+//         gsap.from(".about h1", {
+//             duration: 1,
+//             y: 50,
+//             opacity: 0,
+//             delay: 0.5,
+//             ease: "power3.out",
+//         }, "-=0.8")
+//         gsap.from(".up h2", {
+//             duration: 1.5,
+//             y: 50,
+//             opacity: 0,
+//             ease: "power3.out",
+//         }, "-=0.8")
+//         gsap.from(".up p", {
+//             duration: 1.5,
+//             y: 50,
+//             opacity: 0,
+//             ease: "power3.out",
+//         }, "-=0.8")
+//         gsap.from(".up2 img", {
+//             duration: 1.5,
+//             y: 50,
+//             opacity: 0,
+//             ease: "power3.out",
+//         }, "-=0.8")
+//         gsap.from(".up2 a", {
+//             duration: 1.5,
+//             y: 50,
+//             opacity: 0,
+//             ease: "power3.out",
+//         }, "-=0.8")
+//         gsap.from(".projects h1", {
+//             duration: 1.5,
+//             y: 50,
+//             opacity: 0,
+//             delay: 0.5,
+//             ease: "power3.out",
+//         }, "-=0.8")
+
+//         ;
+// })
